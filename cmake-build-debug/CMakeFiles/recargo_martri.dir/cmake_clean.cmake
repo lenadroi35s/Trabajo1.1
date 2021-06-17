5b847fef11402692f4fe1fc6ce22937cb550e6d2
Leandro Santana <santanaleandro099@gmail.com>
2021-06-17 11:57:17 -0500

    Enunciado del ejercito

---
+++ cmake-build-debug/CMakeFiles/recargo_martri.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/recargo_martri.dir/main.cpp.obj"
+  "librecargo_martri.dll.a"
+  "recargo_martri.exe"
+  "recargo_martri.exe.manifest"
+  "recargo_martri.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recargo_martri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
